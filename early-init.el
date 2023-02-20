c28f4148557ee0d281bb001c1926448000b8d3b9
Sergei Antipov <s.antipov@mulesoft.com>
2023-02-20 15:59:34 -0500

    New config based on Elpaca

--- early-init.el
+++ early-init.el
@@ -1,6 +1,52 @@
-(when (fboundp 'startup-redirect-eln-cache)
-  (startup-redirect-eln-cache
-   (convert-standard-filename
-	(expand-file-name  "var/eln-cache/" user-emacs-directory))))
+;;; early-init.el --- Emacs pre package.el & GUI configuration -*- lexical-binding: t; -*-
+;;; Code:
 
 (setq package-enable-at-startup nil)
+(setq inhibit-default-init nil)
+
+(setq native-comp-async-report-warnings-errors nil)
+
+(defvar default-file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 1)
+
+(defun +gc-after-focus-change ()
+  "Run GC when frame loses focus."
+  (run-with-idle-timer
+   5 nil
+   (lambda () (unless (frame-focus-state) (garbage-collect)))))
+
+(defun +reset-init-values ()
+  (run-with-idle-timer
+   1 nil
+   (lambda ()
+     (setq file-name-handler-alist default-file-name-handler-alist
+           gc-cons-percentage 0.1
+           gc-cons-threshold 100000000)
+     (message "gc-cons-threshold & file-name-handler-alist restored")
+     (when (boundp 'after-focus-change-function)
+       (add-function :after after-focus-change-function #'+gc-after-focus-change)))))
+
+(with-eval-after-load 'elpaca
+  (add-hook 'elpaca-after-init-hook '+reset-init-values))
+
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+
+(setq frame-inhibit-implied-resize t)
+
+(push '(font . "MesloLGS Nerd Font Mono 18") default-frame-alist)
+(set-face-font 'default "MesloLGS Nerd Font Mono 18")
+(set-face-font 'variable-pitch "Helvetica Neue 18")
+(copy-face 'default 'fixed-pitch)
+
+(advice-add #'x-apply-session-resources :override #'ignore)
+
+(setq ring-bell-function #'ignore
+      inhibit-startup-screen t)
+
+(provide 'early-init)
+;;; early-init.el ends here
--- init.el
+++ init.el
@@ -1,86 +1,47 @@
-;;; For performance
-(setq gc-cons-threshold 100000000)
-(setq read-process-output-max (* 1024 1024)) ;; 1mb
+;; -*- lexical-binding: t; -*-
 
+;;; Code:
+(add-hook 'elpaca-after-init-hook
+          (lambda ()
+            (message "Emacs loaded in %s with %d garbage collections."
+                     (format "%.2f seconds"
+                             (float-time
+                              (time-subtract (current-time) before-init-time)))
+                     gcs-done)))
 
-(add-hook 'after-init-hook #'(lambda ()
-                               ;; restore after startup
-                               (setq gc-cons-threshold 800000)))
+;; Uncomment for profiling
+;; (profiler-start 'cpu+mem)
+;; (add-hook 'elpaca-after-init-hook (lambda () (profiler-stop) (profiler-report)))
 
-;;; Disable menu-bar, tool-bar, and scroll-bar.
-(if (fboundp 'menu-bar-mode)
-    (menu-bar-mode -1))
-(if (fboundp 'tool-bar-mode)
-    (tool-bar-mode -1))
-(if (fboundp 'scroll-bar-mode)
-    (scroll-bar-mode -1))
+;; (require 'elp)
+;; (with-eval-after-load file
+;;   (elp-instrument-package file))
+;; (add-hook 'elpaca-after-init-hook
+;;           (lambda () (elp-results) (elp-restore-package (intern file))))
 
-;;; Fix this bug:
-;;; https://www.reddit.com/r/emacs/comments/cueoug/the_failed_to_download_gnu_archive_is_a_pretty/
-(when (version< emacs-version "26.3")
-  (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3"))
+(setq initial-buffer-choice t) ;;*scratch*
 
-;; start the initial frame maximized
-(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+(setq-default tab-width 2
+              c-basic-offset 2
+              kill-whole-line t
+              indent-tabs-mode nil
+              fill-column 119
+              require-final-newline t)
 
-;; start every frame maximized
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
+;;; Lockfiles unfortunately cause more pain than benefit
+(setq create-lockfiles nil)
 
-;;; Setup straight
-(setq-default straight-repository-branch "master"
-              straight-fix-org t
-              straight-fix-flycheck t
-              straight-use-package-by-default t
-              straight-check-for-modifications '(check-on-save find-when-checking))
-(setq vc-follow-symlinks t)
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 6))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-
-(straight-use-package 'use-package)
-
-(use-package use-package-ensure-system-package)
-
-;;; Help keeping .emacs.d clean!
-(use-package no-littering)
-
-(require 'recentf)
-(add-to-list 'recentf-exclude no-littering-var-directory)
-(add-to-list 'recentf-exclude no-littering-etc-directory)
-
-(setq auto-save-file-name-transforms
-	`((".*" ,(no-littering-expand-var-file-name "auto-save/") t)))
-
-(setq custom-file (no-littering-expand-etc-file-name "custom.el"))
-
-;;; Useful Defaults
-(setq inhibit-startup-screen t)           ; Disable startup screen
-(setq initial-scratch-message "")         ; Make *scratch* buffer blank
-(setq-default frame-title-format '("%b")) ; Make window title the buffer name
-(setq ring-bell-function 'ignore)         ; Disable bell sound
-(global-hl-line-mode t)                   ; Enable cursor line highlighting
 (save-place-mode t)                       ; Open file on the last visited position
 (fset 'yes-or-no-p 'y-or-n-p)             ; y-or-n-p makes answering questions faster
-(show-paren-mode 1)                       ; Show closing parens by default
-(setq linum-format "%4d ")                ; Line number format
+(global-hl-line-mode t)                   ; Enable cursor line highlighting
 (delete-selection-mode 1)                 ; Selected text will be overwritten when you start typing
+(electric-pair-mode 1)                    ; auto parens in pairs
 (global-auto-revert-mode t)               ; Auto-update buffer if file has changed on disk
-(add-hook 'before-save-hook
-	  'delete-trailing-whitespace)    ; Delete trailing whitespace on save
+
 (add-hook 'prog-mode-hook                 ; Show line numbers in programming modes
           (if (and (fboundp 'display-line-numbers-mode) (display-graphic-p))
               #'display-line-numbers-mode
             #'linum-mode))
-(setq kill-whole-line t)
 
 ;;; Put Emacs auto-save and backup files to /tmp/ or C:/Temp/
 (defconst emacs-tmp-dir (expand-file-name (format "emacs%d" (user-uid)) temporary-file-directory))
@@ -92,108 +53,84 @@
    version-control t
    backup-directory-alist `((".*" . ,emacs-tmp-dir)))
 
-
 ;; utf-8 everywhere
 (prefer-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
-(set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 
-(electric-pair-mode 1) ; auto parens in pairs
+;; start the initial frame maximized
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
 
-(setq-default
- ;; these settings still should be set on a per language basis, this is just a general default
- indent-tabs-mode nil ; spaces > tabs
- tab-width 4 ; tab is 4 spaces
- fill-column 119
+;; start every frame maximized
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
 
- ;; better security
- gnutls-verify-error t
- gnutls-min-prime-bits 2048
+(defvar elpaca-installer-version 0.1)
+(defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
+(defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
+(defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
+                              :ref nil
+                              :files (:defaults (:exclude "extensions"))
+                              :build (:not elpaca--activate-package)))
+(when-let ((repo  (expand-file-name "repos/elpaca/" elpaca-directory))
+           (build (expand-file-name "elpaca/" elpaca-builds-directory))
+           (order (cdr elpaca-order))
+           ((add-to-list 'load-path (if (file-exists-p build) build repo)))
+           ((not (file-exists-p repo))))
+  (condition-case-unless-debug err
+      (if-let ((buffer (pop-to-buffer-same-window "*elpaca-installer*"))
+               ((zerop (call-process "git" nil buffer t "clone"
+                                     (plist-get order :repo) repo)))
+               (default-directory repo)
+               ((zerop (call-process "git" nil buffer t "checkout"
+                                     (or (plist-get order :ref) "--"))))
+               (emacs (concat invocation-directory invocation-name))
+               ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
+                                     "--eval" "(byte-recompile-directory \".\" 0 'force)"))))
+          (progn (require 'elpaca)
+                 (elpaca-generate-autoloads "elpaca" repo)
+                 (kill-buffer buffer))
+        (error "%s" (with-current-buffer buffer (buffer-string))))
+    ((error) (warn "%s" err) (delete-directory repo 'recursive))))
+(require 'elpaca-autoloads)
+(add-hook 'after-init-hook #'elpaca-process-queues)
+(elpaca `(,@elpaca-order))
 
- mouse-yank-at-point t
- save-interprogram-paste-before-kill t
- apropos-do-all t
- require-final-newline t
- ediff-window-setup-function 'ediff-setup-windows-plain
+(defmacro use-feature (name &rest args)
+  "Like `use-package' but accounting for asynchronous installation.
+NAME and ARGS are in `use-package'."
+  (declare (indent defun))
+  `(elpaca nil (use-package ,name
+                 :ensure nil
+                 ,@args)))
 
- ;; the most reliable tramp setup I have found (used at work every day...)
- tramp-default-method "ssh"
- tramp-copy-size-limit nil
- tramp-use-ssh-controlmaster-options nil
+;; Install use-package support
+(elpaca elpaca-use-package
+  ;; Enable :elpaca use-package keyword.
+  (elpaca-use-package-mode)
+  ;; Assume :elpaca t unless otherwise specified.
+  (setq elpaca-use-package-by-default t))
 
- ;; I recommend the following ~/.ssh/config settings be used with the tramp settings in this cfg:
- ;; Host *
- ;; ForwardAgent yes
- ;; AddKeysToAgent yes
- ;; ControlMaster auto
- ;; ControlPath ~/.ssh/master-%r@%h:%p
- ;; ControlPersist yes
- ;; ServerAliveInterval 10
- ;; ServerAliveCountMax 10
+;; Block until current queue processed.
+(elpaca-wait)
 
- ring-bell-function 'ignore ; be quiet
+(if debug-on-error
+    (setq use-package-verbose t
+          use-package-expand-minimally nil
+          use-package-compute-statistics t)
+  (setq use-package-verbose nil
+        use-package-expand-minimally t))
 
- browse-url-browser-function 'eww-browse-url ; use a text browser --great for clicking documentation links
- )
+(defun +terminal ()
+  "Set the terimnal coding system."
+  (unless (display-graphic-p)
+    (set-terminal-coding-system 'utf-8)))
 
-;;; Lockfiles unfortunately cause more pain than benefit
-(setq create-lockfiles nil)
+(add-hook 'server-after-make-frame-hook #'+terminal)
 
-;;; Modeline
-(use-package doom-modeline
-  :init
-  (doom-modeline-mode 1)
-  :config
-  (setq doom-modeline-height 15))
-
-;;; Theme
 (use-package doom-themes
-  :custom
-  (doom-dracula-brighter-modeline t)
-  (doom-dracula-brighter-comments nil)
-  :custom-face
-  (selectrum-current-candidate ((t (:background "#44475a" :weight bold :foreground "#ff79c6"))))
-  (prescient-primary-highlight ((t :foreground "#ffb86c")))
-  (prescient-secondary-highlight ((t :foreground "#0189cc")))
+  :demand t
   :config
-  (load-theme 'doom-dracula t)
-  (doom-themes-visual-bell-config))
-
-(use-package all-the-icons)
-
-;;; Fonts
-(set-face-attribute 'default nil :font "MesloLGS Nerd Font Mono" :height 190)
-(let ((faces '(mode-line
-               mode-line-buffer-id
-               mode-line-emphasis
-               mode-line-highlight
-               mode-line-inactive)))
-  (mapc
-   (lambda (face) (set-face-attribute face nil :font "MesloLGS Nerd Font Mono" :height 170))
-   faces))
-
-;; Enable rich annotations using the Marginalia package
-(use-package marginalia
-  ;; Either bind `marginalia-cycle' globally or only in the minibuffer
-  :bind (("M-A" . marginalia-cycle)
-         :map minibuffer-local-map
-         ("M-A" . marginalia-cycle))
-
-  ;; The :init configuration is always executed (Not lazy!)
-  :init
-
-  ;; Must be in the :init section of use-package such that the mode gets
-  ;; enabled right away. Note that this forces loading the package.
-  (marginalia-mode))
-
-;;; Blackout
-(use-package blackout)
-
-(use-package undo-tree                    ; Enable undo-tree, sane undo/redo behavior
-  :blackout
-  :init (global-undo-tree-mode)
-  :config (setq undo-tree-auto-save-history nil))
+  (load-theme 'doom-one t))
 
 (use-package tree-sitter
   :config
@@ -206,59 +143,43 @@
 (use-package tree-sitter-langs
   :after tree-sitter)
 
+(use-package all-the-icons
+  :demand t)
+
+(use-package which-key
+  :demand t
+  :init
+  (setq which-key-enable-extended-define-key t)
+  :config
+  (which-key-mode)
+  :custom
+  (which-key-side-window-location 'bottom)
+  (which-key-sort-order 'which-key-key-order-alpha)
+  (which-key-side-window-max-width 0.33)
+  (which-key-idle-delay 0.5)
+  :diminish which-key-mode)
+
+(use-package doom-modeline
+  :defer 2
+  :config
+  (column-number-mode 1)
+  (doom-modeline-mode)
+  :custom
+  (doom-modeline-icon t "Show icons in the modeline"))
+
 (use-package lsp-mode
   :init
   ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
   (setq lsp-keymap-prefix "C-c l")
   :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
-         (javascript-mode . lsp-deferred)
+         (c-mode . lsp-deferred)
          ;; if you want which-key integration
          (lsp-mode . lsp-enable-which-key-integration))
-  :commands lsp lsp-deferred)
-
-;; optional if you want which-key integration
-(use-package which-key
-  :blackout
-  :config
-  (which-key-mode))
-
-(use-package typescript-mode
-  :after tree-sitter
-  :config
-  ;; we choose this instead of tsx-mode so that eglot can automatically figure out language for server
-  ;; see https://github.com/joaotavora/eglot/issues/624 and https://github.com/joaotavora/eglot#handling-quirky-servers
-  (define-derived-mode typescriptreact-mode typescript-mode
-    "TypeScript TSX")
-
-  ;; use our derived mode for tsx files
-  (add-to-list 'auto-mode-alist '("\\.tsx?\\'" . typescriptreact-mode))
-  ;; by default, typescript-mode is mapped to the treesitter typescript parser
-  ;; use our derived mode to map both .tsx AND .ts -> typescriptreact-mode -> treesitter tsx
-  (add-to-list 'tree-sitter-major-mode-language-alist '(typescriptreact-mode . tsx)))
+  :commands (lsp lsp-deferred))
 
 (use-package yaml-mode
   :defer t
   :hook (yaml-mode . lsp-deferred))
-
-(use-package terraform-mode
-  :defer t
-  :hook (terraform-mode . terraform-format-on-save-mode))
-
-(use-package go-mode
-  :defer t
-  :hook (go-mode . lsp-deferred))
-
-(use-package lsp-docker
-  :config
-  (defvar lsp-docker-client-packages
-    '(lsp-clients lsp-go))
-  (setq lsp-docker-client-configs
-        '((:server-id gopls :docker-server-id gopls-docker :server-command "gopls")
-          ))
-  (lsp-docker-init-clients
-   :path-mappings '(("~/go/src/github.com/gravitational/gravity" . "/projects"))
-   :client-packages lsp-docker-client-packages
-   :client-configs lsp-docker-client-configs))
 
 (use-package exec-path-from-shell
   :if (memq window-system '(mac ns))
@@ -267,44 +188,35 @@
   :config
   (exec-path-from-shell-initialize))
 
-(use-package magit)
-
-(use-package selectrum
-  :init
-  (selectrum-mode t)
-  :config
-  (setq prescient-completion-highlight-matches t))
-
-(use-package selectrum-prescient
-  :init
-  ;; to make sorting and filtering more intelligent
-  (selectrum-prescient-mode t)
-
-  ;; to save your command history on disk, so the sorting gets more
-  ;; intelligent over time
-  (prescient-persist-mode t))
-
-;;;;; ctrlf
-;; single-buffer text search in Emacs
-;; [[https://github.com/raxod502/ctrlf#usage][ctrlf]]
-(use-package ctrlf
-  :straight t
-  :init (ctrlf-mode +1))
-
-(use-package groovy-mode)
+(use-package flycheck
+  :init (global-flycheck-mode))
 
 (use-package rg
-  :ensure-system-package
-  (rg . ripgrep))
+  :defer t)
 
-(use-package corfu
-  :custom
-  (corfu-auto t)
-  (corfu-cycle t)
-  :init
-  (global-corfu-mode))
+;; Git related packages
+(use-package git-gutter
+  :defer t
+  :custom (git-gutter:update-interval 0.25)
+  :bind ("C-x g =" . git-gutter:popup-hunk)
+  ("C-x g p" . git-gutter:previous-hunk)
+  ("C-x g n" . git-gutter:next-hunk)
+  :init (global-git-gutter-mode t)
+  (setq git-gutter:modified-sign "~"
+	git-gutter:added-sign "+"
+	git-gutter:deleted-sign "-"))
 
-(setq completion-cycle-threshold 3)
-(setq tab-always-indent 'complete)
-(setq read-extended-command-predicate
-  #'command-completion-default-include-p)
+
+;; Organizing
+(use-package projectile
+  ;; Convenient organization and commands
+  :demand t
+  :config (projectile-mode 1))
+
+(defun lsp-install-save-hooks ()
+  "Setup lsp-related hooks."
+  (add-hook 'before-save-hook #'lsp-format-buffer t t))
+(add-hook 'c-mode-hook #'lsp-install-save-hooks)
+
+(provide 'init)
+;;; init.el ends here
