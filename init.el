48d3e2e956c2c1a13c139887a4d9f156b1a6f1ed
Sergei Antipov <s.antipov@mulesoft.com>
2023-02-24 17:34:05 -0500

    Add vertico and perspective

--- init.el
+++ init.el
@@ -37,6 +37,7 @@
 (delete-selection-mode 1)                 ; Selected text will be overwritten when you start typing
 (electric-pair-mode 1)                    ; auto parens in pairs
 (global-auto-revert-mode t)               ; Auto-update buffer if file has changed on disk
+(savehist-mode t)                         ; Persist history over Emacs restarts. Vertico sorts by history position.
 
 (add-hook 'prog-mode-hook                 ; Show line numbers in programming modes
           (if (and (fboundp 'display-line-numbers-mode) (display-graphic-p))
@@ -127,6 +128,34 @@ NAME and ARGS are in `use-package'."
 
 (add-hook 'server-after-make-frame-hook #'+terminal)
 
+;; A few more useful configurations...
+(use-feature emacs
+  :elpaca nil
+  :init
+  ;; Add prompt indicator to `completing-read-multiple'.
+  ;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
+  (defun crm-indicator (args)
+    (cons (format "[CRM%s] %s"
+                  (replace-regexp-in-string
+                   "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
+                   crm-separator)
+                  (car args))
+          (cdr args)))
+  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
+
+  ;; Do not allow the cursor in the minibuffer prompt
+  (setq minibuffer-prompt-properties
+        '(read-only t cursor-intangible t face minibuffer-prompt))
+  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+
+  ;; Emacs 28: Hide commands in M-x which do not work in the current mode.
+  ;; Vertico commands are hidden in normal buffers.
+  ;; (setq read-extended-command-predicate
+  ;;       #'command-completion-default-include-p)
+
+  ;; Enable recursive minibuffers
+  (setq enable-recursive-minibuffers t))
+
 (use-package doom-themes
   :demand t
   :config
@@ -206,12 +235,51 @@ NAME and ARGS are in `use-package'."
 	git-gutter:added-sign "+"
 	git-gutter:deleted-sign "-"))
 
+;; Enable vertico
+(use-package vertico
+  :init
+  (vertico-mode)
+
+  ;; Different scroll margin
+  ;; (setq vertico-scroll-margin 0)
+
+  ;; Show more candidates
+  ;; (setq vertico-count 20)
+
+  ;; Grow and shrink the Vertico minibuffer
+  ;; (setq vertico-resize t)
+
+  ;; Optionally enable cycling for `vertico-next' and `vertico-previous'.
+  ;; (setq vertico-cycle t)
+  )
+
+;; Optionally use the `orderless' completion style.
+(use-package orderless
+  :init
+  ;; Configure a custom style dispatcher (see the Consult wiki)
+  ;; (setq orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch)
+  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
+  (setq completion-styles '(orderless basic)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles partial-completion)))))
 
 ;; Organizing
 (use-package projectile
   ;; Convenient organization and commands
   :demand t
-  :config (projectile-mode 1))
+  :config (projectile-mode 1)
+  :bind (:map projectile-mode-map
+              ("C-c p" . projectile-command-map)))
+
+(use-package perspective
+  :bind
+  (("C-x C-b" . persp-list-buffers) ; or use a nicer switcher, see below
+   ("C-x b" . persp-switch-to-buffer*)
+   ("C-x k" . persp-kill-buffer*))
+  :custom
+  (persp-mode-prefix-key (kbd "C-c TAB"))  ; pick your own prefix key here
+  :init
+  (persp-mode))
 
 (defun lsp-install-save-hooks ()
   "Setup lsp-related hooks."
